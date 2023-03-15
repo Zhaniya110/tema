f0eafafb289760164cc88354cb8281beb797de19
Zhaniya110 <zmeyramova@gmail.com>
2023-03-15 22:41:48 +0600

    added vital functions

--- wp-content/themes/tema/index.php
+++ wp-content/themes/tema/index.php
@@ -4,3 +4,15 @@
 @package Tema
 */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tema</title>
+</head>
+<body>
+    
+</body>
+</html>
