f0fef85f6d2520ee49eaf719aec278b854c0f900
Zhaniya110 <zmeyramova@gmail.com>
2023-03-16 16:33:39 +0600

    edited header and added a new file from wordpress repo

--- wp-content/themes/tema/footer.php
+++ wp-content/themes/tema/footer.php
@@ -7,6 +7,6 @@
 ?>
 
 
-<footer>Футер</footer>
+        <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/tema/header.php
+++ wp-content/themes/tema/header.php
@@ -11,7 +11,20 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tema</title>
+    <?php wp_head(); ?>
+   
+<?php add_action( 'wp_head', 'hook_css' );
+function hook_css(){
+	echo '<style>.wp_head_example{ background-color : #f1f1f1; } </style>';
+}
+
+## JS в head документа
+add_action( 'wp_head','hook_javascript' );
+function hook_javascript() {
+	echo "<script> alert('Page is loading...'); </script>";
+}
+?>
 </head>
 <body>
     <header>Хедер</header>
+
